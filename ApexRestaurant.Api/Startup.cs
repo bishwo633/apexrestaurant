4c66ea67bb9428903cd29294948326596a645f13
Bishwo <bishwo.adhikari@gmail.com>
2019-01-13 20:46:51 +0545

    added api controller for customer

--- ApexRestaurant.Api/Startup.cs
+++ ApexRestaurant.Api/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApexRestaurant.Repository;
+using ApexRestaurant.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,10 @@ namespace ApexRestaurant.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            RepositoryModule.Register(services, 
+                Configuration.GetConnectionString("DefaultConnection"),
+                GetType().Assembly.FullName);
+            ServicesModule.Register(services);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,6 +47,7 @@ namespace ApexRestaurant.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
